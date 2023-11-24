ce263174932450aabf0a46186ce78af6980b8134
John Linebarger <gearclubnewmexico@gmail.com>
2023-11-24 08:57:20 -0700

    Updated project and added documentation after the fork

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# FTC WIRES Autonomous based on Road Runner 1.8
+# GearMasters fork of FTC WIRES fork of acmerobotics implementation of Road Runner 1.8 with examples of autonomous programs
 For instructions and documentation, please register at https://forms.gle/mBFYMgsE5pH3QBXa9
 For details, check out http://www.ftcwires.org/softwareplatform
-
-
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tutorial.java
@@ -0,0 +1,38 @@
+// Tutorial.java
+//
+// Progressive tutorial file based on playlist from Artemis Robotics.
+// https://www.youtube.com/playlist?list=PLnyefHqkc2K-gQD4hO5x3IcANuDuNMKDP (22-23 season)
+// https://github.com/artemis18715/New-Programming-Tutorial-23-24 (23-24 season)
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp(name = "Tutorial Test")
+public class Tutorial extends OpMode {
+
+    DcMotor motor;
+
+    @Override
+    public void init() {
+        // Send text to driver hub/phone
+        telemetry.addData( "Initialization:", "Success!");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            telemetry.addData( "GamePad:", "The a button is pressed.");
+            telemetry.update();
+        }
+
+        if (gamepad1.b) {
+            telemetry.addData( "GamePad:", "The b button is pressed.");
+            telemetry.update();
+        }
+
+    }
+
+}
